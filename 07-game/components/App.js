e8c691886116cd5a3c5dcbe21da4129390c737f9
Chris Hitchcott <hitchcott@gmail.com>
2020-12-24 17:54:26 +0700

    Add collision

--- 07-game/components/App.js
+++ 07-game/components/App.js
@@ -1,18 +1,13 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import "@babylonjs/core/Physics/physicsEngineComponent"; // side-effect adds scene.enablePhysics function
-import {
-  Vector3,
-  PhysicsImpostor,
-  Mesh,
-  Color3,
-  FresnelParameters,
-  Texture,
-  WebXRDefaultExperience,
-} from "@babylonjs/core";
+import { Vector3, PhysicsImpostor } from "@babylonjs/core";
 import { CannonJSPlugin } from "@babylonjs/core/Physics/Plugins";
-import { Scene, Engine, useBabylonScene } from "react-babylonjs";
+import { Scene, Engine } from "react-babylonjs";
 
 import * as CANNON from "cannon";
+// import Snowballs from "./Snowballs";
+import Dude from "./Dude";
+import Inspector from "./Inspector";
 import Snowballs from "./Snowballs";
 window.CANNON = CANNON;
 
@@ -30,9 +25,11 @@ const App = () => {
   return (
     <Engine antialias={true} adaptToDeviceRatio={true} canvasId="canvas">
       <Scene
+        collisionsEnabled
         enablePhysics={[gravityVector, new CannonJSPlugin()]}
         onMeshPicked={meshPicked}
       >
+        {/* <Inspector /> */}
         <arcRotateCamera
           name="arc"
           target={new Vector3(0, 1, 0)}
@@ -42,7 +39,7 @@ const App = () => {
           minZ={0.001}
           wheelPrecision={50}
           lowerRadiusLimit={8}
-          upperRadiusLimit={20}
+          upperRadiusLimit={200}
           upperBetaLimit={Math.PI / 2}
         />
         <hemisphericLight
@@ -64,89 +61,26 @@ const App = () => {
             useBlurExponentialShadowMap={true}
             blurKernel={32}
             darkness={0.8}
-            shadowCasters={["sphere1", "dialog"]}
+            shadowCasters={["dude", "sphere"]}
             forceBackFacesOnly={true}
             depthScale={100}
           />
         </directionalLight>
-        <sphere
-          name="sphere1"
-          diameter={2}
-          segments={16}
-          position={new Vector3(0, 2.5, 0)}
-        >
-          <physicsImpostor
-            type={PhysicsImpostor.SphereImpostor}
-            _options={{ mass: 1, restitution: 0.9 }}
-          />
-          <standardMaterial
-            name="material1"
-            specularPower={16}
-            diffuseColor={Color3.Black()}
-            emissiveColor={new Color3(0.5, 0.5, 0.5)}
-            reflectionFresnelParameters={FresnelParameters.Parse({
-              isEnabled: true,
-              leftColor: [1, 1, 1],
-              rightColor: [0, 0, 0],
-              bias: 0.1,
-              power: 1,
-            })}
-          />
-          <plane
-            name="dialog"
-            size={2}
-            position={new Vector3(0, 1.5, 0)}
-            sideOrientation={Mesh.BACKSIDE}
-          >
-            <advancedDynamicTexture
-              name="dialogTexture"
-              height={1024}
-              width={1024}
-              createForParentMesh={true}
-              hasAlpha={true}
-              generateMipMaps={true}
-              samplingMode={Texture.TRILINEAR_SAMPLINGMODE}
-            >
-              <rectangle
-                name="rect-1"
-                height={0.5}
-                width={1}
-                thickness={12}
-                cornerRadius={12}
-              >
-                <rectangle>
-                  <babylon-button
-                    name="close-icon"
-                    background="green"
-                    // onPointerDownObservable={onButtonClicked}
-                  >
-                    <textBlock
-                      text={"\uf00d click me"}
-                      fontFamily="FontAwesome"
-                      fontStyle="bold"
-                      fontSize={200}
-                      color="white"
-                    />
-                  </babylon-button>
-                </rectangle>
-              </rectangle>
-            </advancedDynamicTexture>
-          </plane>
-        </sphere>
 
         <ground
           name="ground"
-          width={10}
-          height={10}
+          width={100}
+          height={100}
           subdivisions={2}
           receiveShadows={true}
         >
           <physicsImpostor
             type={PhysicsImpostor.BoxImpostor}
-            _options={{ mass: 0, restitution: 0.9 }}
+            _options={{ mass: 0, restitution: 0.6 }}
           />
         </ground>
         <Snowballs />
+        <Dude />
       </Scene>
     </Engine>
   );
--- 07-game/components/Snowballs.js
+++ 07-game/components/Snowballs.js
@@ -4,9 +4,12 @@ import {
   WebXRDefaultExperience,
   Animation,
   BezierCurveEase,
+  Color3,
+  FresnelParameters,
 } from "@babylonjs/core";
 import { useEffect, useRef } from "react";
-import { useBabylonScene } from "react-babylonjs";
+import { Mesh, Texture, useBabylonScene } from "react-babylonjs";
+import { PhysicsImpostor } from "@babylonjs/core";
 
 let observers = {};
 let meshesUnderPointer = {};
@@ -17,6 +20,79 @@ tmpRay.origin = new Vector3();
 tmpRay.direction = new Vector3();
 let lastTimestamp = 0;
 const oldPos = new Vector3();
+
+function Snowall({ position }) {
+  const sphereRef = useRef();
+  const scene = useBabylonScene();
+
+  function resetPos(mesh) {
+    mesh.position.x = position.x;
+    mesh.position.y = position.y;
+    mesh.position.z = position.z;
+    mesh.physicsImpostor.setLinearVelocity(Vector3.Zero());
+    mesh.physicsImpostor.setAngularVelocity(Vector3.Zero());
+  }
+  scene.registerBeforeRender(() => {
+    const mesh = sphereRef.current.hostInstance;
+    if (mesh.position.y < -0.1) {
+      resetPos(mesh);
+    }
+    // const dude = scene.getMeshByName("dude");
+    scene.meshes
+      .filter((m) => m.name == "dude")
+      .forEach((m) => {
+        if (mesh.intersectsMesh(m, true)) {
+          console.log("bam");
+          resetPos(mesh);
+        }
+      });
+  });
+  return (
+    <sphere
+      ref={sphereRef}
+      name="sphere"
+      diameter={0.2}
+      checkCollisions
+      onCollide={() => console.log("yo")}
+      segments={16}
+      position={position}
+    >
+      <physicsImpostor
+        type={PhysicsImpostor.SphereImpostor}
+        _options={{ mass: 1, restitution: 0.9 }}
+      />
+      <standardMaterial
+        name="material1"
+        specularPower={16}
+        diffuseColor={Color3.Black()}
+        emissiveColor={new Color3(0.5, 0.5, 0.5)}
+        reflectionFresnelParameters={FresnelParameters.Parse({
+          isEnabled: true,
+          leftColor: [1, 1, 1],
+          rightColor: [0, 0, 0],
+          bias: 0.1,
+          power: 1,
+        })}
+      />
+      <plane
+        name="dialog"
+        size={2}
+        position={new Vector3(0, 1.5, 0)}
+        sideOrientation={Mesh.BACKSIDE}
+      >
+        <advancedDynamicTexture
+          name="dialogTexture"
+          height={1024}
+          width={1024}
+          createForParentMesh={true}
+          hasAlpha={true}
+          generateMipMaps={true}
+          samplingMode={Texture.TRILINEAR_SAMPLINGMODE}
+        />
+      </plane>
+    </sphere>
+  );
+}
 
 export default function Snowballs() {
   const scene = useBabylonScene();
@@ -39,7 +115,7 @@ export default function Snowballs() {
                   if (squeeze.pressed) {
                     // animate position
                     controller.getWorldPointerRayToRef(tmpRay, true);
-                    tmpRay.direction.scaleInPlace(1.5);
+                    tmpRay.direction.scaleInPlace(0.2);
                     const position = controller.grip
                       ? controller.grip.position
                       : controller.pointer.position;
@@ -75,7 +151,7 @@ export default function Snowballs() {
                                 xr.baseExperience.sessionManager
                                   .currentTimestamp;
                               controller.getWorldPointerRayToRef(tmpRay, true);
-                              tmpRay.direction.scaleInPlace(1.5);
+                              tmpRay.direction.scaleInPlace(0.2);
                               const position = controller.grip
                                 ? controller.grip.position
                                 : controller.pointer.position;
@@ -136,7 +212,13 @@ export default function Snowballs() {
 
   return (
     <>
-      <box />
+      {new Array(20).fill(null).map((n, i) => (
+        <Snowall
+          key={i}
+          position={new Vector3(i * 0.01 * 3 - 3, 0.2 + i * 0.1, -3)}
+        />
+      ))}
+      <Snowall position={new Vector3(0, 10, 0)} />
     </>
   );
 }
